b20df53d1e6b437cc8803335ac83d535f75e88b2
xudacheng <xudacheng_tomous@163.com>
2018-09-27 16:34:29 +0800

    Initial Commit

---
+++ 物流进度demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  物流进度demo
+//
+//  Created by 大橙子 on 2018/9/27.
+//  Copyright © 2018年 中都格罗唯视. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 物流进度demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  物流进度demo
+//
+//  Created by 大橙子 on 2018/9/27.
+//  Copyright © 2018年 中都格罗唯视. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
